4c5346353e14346c7107deb90a8825dba2c68b9f
Les McWhirter <leslie.mcwhirter@gmail.com>
2025-03-19 18:30:33 -0700

    Linter-5

--- src/InfrastructureAsCode/vnet.tf
+++ src/InfrastructureAsCode/vnet.tf
@@ -40,6 +40,35 @@ resource "azurerm_subnet" "subnet" {
   }
 }
 
+resource "azurerm_network_security_group" "subnet_nsg" {
+  count = length(var.vnet_subnets)
+  name  = format("%s-%s-%s-nsg", local.base-name, "subnet", var.vnet_subnets[count.index].Name)
+  resource_group_name = azurerm_resource_group.deployment-rg[0].name
+  location = azurerm_resource_group.deployment-rg[0].location
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  count = length(var.vnet_subnets)
+
+  subnet_id                 = azurerm_subnet.subnet[count.index].id
+  network_security_group_id = azurerm_network_security_group.subnet_nsg[count.index].id
+}
+
+resource "azurerm_network_security_rule" "example" {
+  name                       = "AllowHTTP"
+  priority                   = 100
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "80"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+
+  resource_group_name         =  azurerm_resource_group.deployment-rg[0].name
+  network_security_group_name = azurerm_network_security_group.subnet_nsg[0].name
+}
+
 resource "azurerm_monitor_diagnostic_setting" "vnet_diagnostics" {
   name = format("%s-%s", local.base-name, "vnet-ds")
 
